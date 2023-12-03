ce0986230e6c75b5bd544efd36b9d64779af05a3
Argenisn47 <148662247+Argenisn47@users.noreply.github.com>
2023-12-03 00:09:09 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ db.run(category, (err) => {
 
 
 /*Vista interfaz administrativa*/
-router.get("/admin", (req, res) => {
+router.get("/adminInterface", (req, res) => {
   db.all(`SELECT * FROM products`, [], (err, product) => {
     db.all(`SELECT * FROM categorys`, [], (err, category) => {
       db.all(`SELECT * FROM images`, [], (err, img) => {
@@ -43,7 +43,7 @@ router.get("/admin", (req, res) => {
 router.get('/deleteProduct/:id',(req,res) => {
   const sqlQuery = "DELETE FROM products WHERE id = ?";
   db.run(sqlQuery,req.params.id,(err,row) => {
-    err ? err:res.redirect('/admin')
+    err ? err:res.redirect('/adminInterface')
   })
 })
 
@@ -67,7 +67,7 @@ router.post('/addProduct', (req, res) => {
   VALUES (?,?,?,?,?,?,?)`, [name, code, price, estado, envio, description, idCategory], (err) => {
     console.log(err);
     db.run(`INSERT INTO images (producto_id,url,destacado) VALUES (?,?,?)`, ['', 'https://upload.wikimedia.org/wikipedia/commons/0/0a/No-image-available.png', destacado], (err) => {
-      err ? console.log(err) : res.redirect('/admin');
+      err ? console.log(err) : res.redirect('/adminInterface');
     })
   })
 })
@@ -84,7 +84,7 @@ router.get("/addCategory", (req, res) => {
 router.post("/addCategory", (req, res) => {
   const { categoryProduct } = req.body;
   db.run(`INSERT INTO categorys (nombre) VALUES (?);`, [categoryProduct], (err) => {
-    err ? console.log(err) : res.redirect('/admin')
+    err ? console.log(err) : res.redirect('/adminInterface')
   })
 })
 
@@ -102,7 +102,7 @@ router.get("/editCategory", (req,res) => {
 router.post("/editCategory/:id",(req,res) => {
   const { id } = req.params;
   db.run(`UPDATE categorys SET nombre = ? WHERE id = ?`,[req.body.categoryProduct,id],(err) => {
-    err ? console.log(err) : res.redirect('/admin')
+    err ? console.log(err) : res.redirect('/adminInterface')
   })
 })
 
@@ -125,11 +125,12 @@ router.get("/addImage/:id", (req, res) => {
 router.post("/addImage/:id", (req, res) => {
   const destacado = 1;
   const img = req.body.imgProduct;
-  db.run(`UPDATE images SET producto_id = ?, url = ?, destacado = ? WHERE ( id = ?)`, [req.params.id, img, destacado, req.params.id], (err) => {
+  const { id } = req.params;
+  db.run(`UPDATE images SET producto_id = ?, url = ?, destacado = ? WHERE ( id = ?)`, [id, img, destacado, id], (err) => {
     if (err) {
       console.log(err)
     } else {
-      res.redirect('/admin');
+      res.redirect('/adminInterface');
     }
   })
 })
@@ -156,17 +157,93 @@ router.post('/editProduct/:id',(req,res) => {
   })
 })
 
-router.get('/',(req,res) => {
-  res.render('index.ejs')
+router.get('/admin',(req,res) => {
+  res.render('indexLogin.ejs')
 })
 
 router.post("/login",(req,res) => {
   const { user,password } = req.body;
   if(user == process.env.USER && password == process.env.USER_PASSWORD){
-    res.redirect('/admin');
+    res.redirect('/adminInterface');
   }else{
     res.redirect('/')
   }
 })
+
+
+router.get("/", (req, res) => {
+  db.all(`SELECT * FROM products`, [], (err, queryProduct) => {
+    db.all(`SELECT * FROM categorys`, [], (err, queryCategory) => {
+      db.all(`SELECT * FROM images`, [], (err, queryImg) => {
+        res.render('index', {
+          Product: queryProduct,
+          Productcategory: queryCategory,
+          Productimg: queryImg
+        })
+      })
+    })
+  })
+})
+
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.get(`SELECT * FROM products WHERE id = ?`, [id], (err, queryProduct) => {
+    db.all(`SELECT * FROM categorys`, [], (err, queryCategory) => {
+      db.get(`SELECT * FROM images WHERE producto_id = ?`, [id], (err, queryImg) => {
+        res.render('searchProduct', {
+          Product: queryProduct,
+          Productcategory: queryCategory,
+          Productimg: queryImg
+        })
+      })
+    })
+  })
+})
+
+
+
+router.get('/categoryid/:id', (req, res) => {
+  const { id } = req.params;
+  const sqlCategory = "SELECT * FROM categorys"
+  const sqlQuery = "SELECT products.*, images.url FROM products LEFT JOIN images ON products.id = images.producto_id WHERE products.categoria_id = ?";
+  db.all(sqlQuery, id, (err, product) => {
+    db.all(sqlCategory, [], (err, category) => {
+      res.render('index', {
+        Product: product,
+        Productcategory: category,
+        Productimg: product
+      })
+    })
+  })
+})
+
+
+router.post('/filtro',(req,res) => {
+  const { name, estado, envio } = req.body;
+  const sqlQuery = "SELECT products.*, images.url FROM products LEFT JOIN images ON products.id = images.producto_id WHERE products.nombre = ? OR products.estado = ? OR products.envio = ?"
+  const sqlCategory = "SELECT * FROM categorys";
+  db.all(sqlQuery,[name,estado,envio],(err,product) => {
+    console.log(product)
+    db.all(sqlCategory,[],(err,category) => {
+      res.render('index',{
+        Product: product,
+        Productcategory: category,
+        Productimg: product
+      })
+    })
+  })
+})
+
+
+router.post("/login", (req, res) => {
+  const { user, password } = req.body;
+  if(user == process.env.USER && password == process.env.USER_PASSWORD){
+    res.redirect('/admin');
+  } else {
+    res.redirect('/');
+  }
+})
+
 
 module.exports = router;
